b7846c574c634e679422697eae22beb80143b4bf
Stephane Monier <smonier@Jahia.com>
2025-06-17 16:31:36 +0200

    feat: add logging utility

---
+++ src/javascript/log.js
@@ -0,0 +1,36 @@
+const isDebugEnabled = process.env.NODE_ENV !== 'production';
+
+const debug = (...args) => {
+    if (isDebugEnabled) {
+        if (window?.jahia?.log?.debug) {
+            window.jahia.log.debug(...args);
+        } else if (console.debug) {
+            console.debug(...args);
+        } else {
+            console.log(...args);
+        }
+    }
+};
+
+const info = (...args) => {
+    if (window?.jahia?.log?.info) {
+        window.jahia.log.info(...args);
+    } else if (console.info) {
+        console.info(...args);
+    } else {
+        console.log(...args);
+    }
+};
+
+const error = (...args) => {
+    if (window?.jahia?.log?.error) {
+        window.jahia.log.error(...args);
+    } else {
+        console.error(...args);
+    }
+};
+
+window.jahia = window.jahia || {};
+window.jahia.log = window.jahia.log || {debug, info, error};
+
+export default {debug, info, error};
